5ed67b17dfd1e194f89560bedba3e67959404ce6
Mahamed Hassan <mdooy66@gmail.com>
2020-11-15 11:26:25 -0500

    updating our up with ashop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1> HATS PAGE </h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 function App() {
-   return (
-   <div>
-     <switch>
-      <Route exact path='/' component={HomePage} />
-      <Route path='/hats' component={HatsPage } />
-     </switch>
-    
-   </div>
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
