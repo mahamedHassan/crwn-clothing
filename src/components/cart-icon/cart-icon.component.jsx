bc051255b04e4b7013270c14d803c5803b407d66
Mahamed Hassan <mdooy66@gmail.com>
2020-11-23 09:58:55 -0500

    added checkou page, checkout item component and updated cart to use selectors from reselect

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
-//import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
+
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className='cart-icon' onClick={toggleCartHidden}>
@@ -19,11 +21,9 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-const mapStateToProps = ({cart: { cartItems }}) => ({
-  itemCount: cartItems.reduce(
-      (accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity,
-      0
-  )
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemsCount
+  
 });
 
 export default connect(
